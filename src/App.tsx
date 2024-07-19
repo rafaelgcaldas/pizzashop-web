08cd890446e5f54d00efab3c36839f8cf1cf47b9
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-07-18 23:10:38 -0300

    feat: eslintrc config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './global.css'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className="flex justify-center items-center h-screen">
       <Button>Enviar</Button>
     </div>
   )
